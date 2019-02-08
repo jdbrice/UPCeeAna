9a1b2b1e4a3b6c8805fc3ca7165451979c1bd154
Daniel Brandenburg <jdb12@rice.edu>
2019-02-08 08:56:32 -0500

    added scripts for more cos4phi analysis

---
+++ bin/allM_cosphi.C
@@ -0,0 +1,109 @@
+
+
+
+void allM_cosphi( string n = "Run10AuAu.root" ){
+
+	TFile *f = new TFile( n.c_str() );
+
+	TH1 * h1=(TH1*)f->Get("wdeltaPhi_pd1_fold_all");
+	TH1 * h0 = (TH1*)h1->Clone("h0");
+	TH1 * h3=(TH1*)f->Get("wdeltaPhi_pd1_2fold_all");
+	
+	
+	gStyle->SetOptFit(111);
+	
+	// gStyle->SetStatW(0.4);
+
+	TCanvas *c1 = new TCanvas( "c1", "", 1200, 900 );
+	h0->Draw("pe");
+	TF1 * f0 = new TF1( "f2", "[0] + [1] * cos( 4 * x )" );
+	h0->Fit( f0, "R", "", 0, 1.55 );
+
+	TCanvas *c2 = new TCanvas( "c2", "", 1200, 900 );
+	h1->Draw("pe");
+	TAxis* a = h1->GetXaxis();
+	a->SetLabelOffset(0.02);
+	a->SetNdivisions(-502);
+	a->ChangeLabel(2,-1,-1,-1,-1,-1,"#frac{#pi}{4}");
+	a->ChangeLabel(-1,-1,-1,-1,-1,-1,"#frac{#pi}{2}");
+
+	// TF1 * f1 = new TF1( "f2", "[0] + [1] * cos( 4 * x ) +  [2] * cos( 2*x )" );
+	TF1 * f1 = new TF1( "f2", "[0] *( 1 + [1] * cos( 4 * x ) +  [2] * cos( 2*x ) )" );
+	f1->SetParName( 1, "4 #phi (%)" );
+	f1->SetParName( 2, "2 #phi (%)" );
+	h1->Fit( f1, "R", "", 0, 1.55 );
+
+	cout << "Asym: " << f1->GetParameter(1) / f1->GetParameter(0) << " +/- " << f1->GetParError(1) / f1->GetParameter(0) << endl;
+	cout << "Relative Error: " << (f1->GetParError(1) / f1->GetParameter(0)) / (f1->GetParameter(1) / f1->GetParameter(0)) << endl;
+	cout << "nSigma : " << (f1->GetParameter(1) / f1->GetParameter(0)) / (f1->GetParError(1) / f1->GetParameter(0)) << endl;
+
+	c2->Print( "cos4phi_allM.png" );
+	c2->Print( "/Users/jdb/notable/attachments/upcee_deltaPhi_foldpi2_allM.png" );
+
+
+	TCanvas *c4 = new TCanvas( "c4", "", 1200, 900 );
+	c4->SetBottomMargin( 0.15 );
+	c4->SetLeftMargin( 0.15 );
+	c4->SetRightMargin( 0.05 );
+	gStyle->SetStatX(0.55);
+	TF1 * f3 = new TF1( "f3", "[0] + [1] * cos( 4 * x )" );
+	// h2->Rebin(4);
+	h3->SetTitle( ";#phi^{ee} - #phi^{e}; dN/d#Delta#phi" );
+	// h3->GetXaxis()->CenterTitle();
+	h3->GetXaxis()->SetTitleOffset( 1.4 );
+	h3->GetYaxis()->SetTitleOffset( 1.4 );
+
+	f3->SetParName( 1, "4 #phi" );
+	f3->SetParName( 2, "2 #phi" );
+	h3->Draw("pe");
+	h3->Fit( f3, "RE", "", 0, TMath::Pi()/4.0 );
+
+	a = h3->GetXaxis();
+	a->SetLabelOffset(0.02);
+	a->SetNdivisions(-502);
+	a->ChangeLabel(2,-1,-1,-1,-1,-1,"#frac{#pi}{8}");
+	a->ChangeLabel(-1,-1,-1,-1,-1,-1,"#frac{#pi}{4}");
+
+
+
+	cout << "Asym: " << (f3->GetParameter(1) / f3->GetParameter(0)) << " +/- " << (f3->GetParError(1) / f3->GetParameter(0)) << endl;
+	cout << "Relative Error: " << (f3->GetParError(1) / f3->GetParameter(0)) / (f3->GetParameter(1) / f3->GetParameter(0)) << endl;
+	cout << "nSigma : " << (f3->GetParameter(1) / f3->GetParameter(0)) / (f3->GetParError(1) / f3->GetParameter(0)) << endl;
+
+	c4->Print( "/Users/jdb/notable/attachments/upcee_deltaPhi_foldpi4_allM.png" );
+
+
+
+
+
+	TCanvas *c5 = new TCanvas( "c5", "", 1200, 900 );
+	c5->SetBottomMargin( 0.15 );
+	c5->SetLeftMargin( 0.15 );
+	c5->SetRightMargin( 0.05 );
+	gStyle->SetStatX(0.55);
+	TF1 * f4 = new TF1( "f4", "[0] *( 1 + [1] * cos( 4 * x ))" );
+	// h2->Rebin(4);
+	h3->SetTitle( ";#phi^{ee} - #phi^{e}; dN/d#Delta#phi" );
+	// h3->GetXaxis()->CenterTitle();
+	h3->GetXaxis()->SetTitleOffset( 1.4 );
+	h3->GetYaxis()->SetTitleOffset( 1.4 );
+
+	f4->SetParName( 1, "4 #phi (%)" );
+	h3->Draw("pe");
+	h3->Fit( f4, "RE", "", 0, TMath::Pi()/4.0 );
+
+	a = h3->GetXaxis();
+	a->SetLabelOffset(0.02);
+	a->SetNdivisions(-502);
+	a->ChangeLabel(2,-1,-1,-1,-1,-1,"#frac{#pi}{8}");
+	a->ChangeLabel(-1,-1,-1,-1,-1,-1,"#frac{#pi}{4}");
+
+
+
+	cout << "Asym: " << (f4->GetParameter(1) / f4->GetParameter(0)) << " +/- " << (f4->GetParError(1) / f4->GetParameter(0)) << endl;
+	cout << "Relative Error: " << (f4->GetParError(1) / f4->GetParameter(0)) / (f4->GetParameter(1) / f4->GetParameter(0)) << endl;
+	cout << "nSigma : " << (f4->GetParameter(1) / f4->GetParameter(0)) / (f4->GetParError(1) / f4->GetParameter(0)) << endl;
+
+	c5->Print( "/Users/jdb/notable/attachments/upcee_deltaPhi_foldpi4_frac_allM.png" );
+
+}
--- modules/Analyzer.h
+++ modules/Analyzer.h
@@ -24,6 +24,8 @@
 #include "FemtoPairFormat/FemtoPair.h"
 
 #include <algorithm>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 
 class Analyzer : public TreeAnalyzer
@@ -32,7 +34,10 @@ protected:
 
 	BranchReader<FemtoPair> _fpr;
 	float dTofCut = 0.2;
+	float XeeXpipi = 5;
+
 	float XeeCut = 0.2;
+	float XpipiCut = 0.0;
 	float ScaleFactor = 1.0;
 
 	float ZDCMin = 0;
@@ -47,7 +52,22 @@ protected:
 	TH2 * hEffMc = nullptr;
 	TH2 * hEff   = nullptr;
 	TH2 * hRapEff  = nullptr;
+	TH2 * hCosThetaEff  = nullptr;
 	TH3 * hEff3D  = nullptr;
+
+	TH2 * hSLEff   = nullptr;
+
+	TH1 * hSampleMassEff = nullptr;
+	TH1 * hSampleMassEffw = nullptr;
+
+	TH1 * hVPMassEff = nullptr;
+	TH1 * hSLMassEff = nullptr;
+
+
+	// Tracking cuts
+	int min_nHitsFit  = 20;
+	int min_nHitsDedx = 10;
+	float max_Dca = 1.0;
 
 	
 
@@ -63,7 +83,10 @@ public:
 		this->_fpr.setup( this->chain, "Pairs" );
 
 		dTofCut = config.get<float>( "p.dTofCut", 0.2 );
+		XeeXpipi = config.get<float>( "p.XeeXpipi", 5 );
+
 		XeeCut = config.get<float>( "p.XeeCut", 5 );
+		XpipiCut = config.get<float>( "p.XpipiCut", 0 );
 
 		ZDCEastMin = config.get<float>( "p.ZDCEast:min", 50 );
 		ZDCEastMax = config.get<float>( "p.ZDCEast:max", 1200 );
@@ -81,28 +104,49 @@ public:
 		// }
 
 		LOG_F( INFO, "dTofCut = %f", dTofCut );
+		
+		LOG_F( INFO, "XeeXpipi = %f", XeeXpipi );
 		LOG_F( INFO, "XeeCut = %f", XeeCut );
+		// LOG_F( INFO, "XpipiCut = %f", XpipiCut );
 
 		// LOG_F( INFO, "ZDC Cuts = ( %f, %f )", ZDCMin, ZDCMax );
 		LOG_F( INFO, "ZDCEast Cuts = ( %f, %f )", ZDCEastMin, ZDCEastMax );
 		LOG_F( INFO, "ZDCWest Cuts = ( %f, %f )", ZDCWestMin, ZDCWestMax );
-
-		// if ( "Run12UU" == config.get<string>("mod") )
-		// 	ScaleFactor = 1.0 / 5.50807e6;
-		// else
-		// 	ScaleFactor = 1.0 / 4.7787e6;
-
 		LOG_F( INFO, "1.0/ScaleFactor( %s ) = %f", config.get<string>("mod").c_str(), 1.0 / ScaleFactor );
 
 		gRandom = new TRandom3();
 		gRandom->SetSeed( 0 );
 
+		min_nHitsFit  = config.get<int>( "p.min_nHitsFit", 20 );
+		min_nHitsDedx = config.get<int>( "p.min_nHitsDedx", 10 );
+		max_Dca       = config.get<float>( "p.max_Dca", 1.0 );
+		
+		LOG_F( INFO, "min_nHitsFit = %d" , min_nHitsFit  );
+		LOG_F( INFO, "min_nHitsDedx = %d" , min_nHitsDedx  );
+		LOG_F( INFO, "max_Dca = %f", max_Dca );
+		
 
 		TFile *f = new TFile( "/Users/jdb/bnl/work/upc/data/Cen60_80_VP_pairEff.root" );
 		hEffMc = (TH2*)f->Get("hMCAcc1virtualphotoneehist");
 		hEffRc = (TH2*)f->Get("hRCvirtualphotoneehistRun10");
 		hEff = (TH2*)hEffRc->Clone( "hTpcEff" );
 		hEff->Divide( hEffMc );
+
+		TH1 * hVPMassEffRc = hEffRc->ProjectionY( "hVPMassEffRc", hEffRc->GetXaxis()->FindBin(0.050), hEffRc->GetXaxis()->FindBin(0.150) );
+		TH1 * hVPMassEffMc = hEffMc->ProjectionY( "hVPMassEffMc", hEffMc->GetXaxis()->FindBin(0.050), hEffMc->GetXaxis()->FindBin(0.150) );
+		hVPMassEff = (TH1*)hVPMassEffRc->Clone( "hVPMassEff" );
+		hVPMassEff->Divide( hVPMassEffMc );
+
+		TFile *fSLEff = new TFile( "/Users/jdb/bnl/work/upc/data/Cen60_80_GammaGamma_pairEff.root" );
+		TH2 * hSLEffMc = (TH2*)fSLEff->Get("hMCAcc1gammagammaeehist");
+		TH2 * hSLEffRc = (TH2*)fSLEff->Get("hRCgammagammaeehistRun10");
+		hSLEff = (TH2*)hSLEffRc->Clone( "hSLEff" );
+		hSLEff->Divide( hSLEffMc );
+
+		TH1 * hSLMassEffRc = hSLEffRc->ProjectionY( "hSLMassEffRc", hSLEffRc->GetXaxis()->FindBin(0.050), hSLEffRc->GetXaxis()->FindBin(0.150) );
+		TH1 * hSLMassEffMc = hSLEffMc->ProjectionY( "hSLMassEffMc", hSLEffMc->GetXaxis()->FindBin(0.050), hSLEffMc->GetXaxis()->FindBin(0.150) );
+		hSLMassEff = (TH1*)hSLMassEffRc->Clone( "hSLMassEff" );
+		hSLMassEff->Divide( hSLMassEffMc );
 
 		LOG_F( INFO, "Making 3D eff factor" );
 		TFile *fRap = new TFile( "/Users/jdb/bnl/work/upc/data/lowPt_EffvsRapidity_Cen6080.root" );
@@ -120,9 +164,22 @@ public:
 		hEff3D = (TH3*)hRap3EffRc->Clone("hEff3D");
 		hEff3D->Divide( hRap3EffMc );
 
+		TH3 * hCosThetaEffMc = (TH3*)fRap->Get("hMCAcc1CosThetavsMee_loweePt");
+		TH3 * hCosThetaEffRc = (TH3*)fRap->Get("hRCAcc1CosThetavsMeeRun10_loweePt");
+		LOG_F( INFO, "CosTheta eff tables: %p, %p", hCosThetaEffMc, hCosThetaEffRc );
 
+		hCosThetaEff = (TH2*)hCosThetaEffRc->Clone( "hTpcEffCosTheta" );
+		hCosThetaEff->Divide( hCosThetaEffMc );
 		
 		book->cd();
+
+		hVPMassEff->Write();
+		hSLMassEff->Write();
+
+		hSampleMassEff = (TH1*)hSLMassEff->Clone( "hSampleMassEff" );
+		hSampleMassEff->Reset();
+		hSampleMassEffw = (TH1*)hSLMassEff->Clone( "hSampleMassEffw" );
+		hSampleMassEffw->Reset();
 
 		hEffMc->Write();
 		hEffRc->Write();
@@ -139,7 +196,79 @@ protected:
 		int ip = ax->FindBin( p );
 		int im = ay->FindBin( m );
 
-		return 1.0 / ( hEff->GetBinContent( ip, im ) );
+		float v = hEff->GetBinContent( ip, im );
+		if ( 0 == v || 1.0/v != 1.0/v )
+			return 0;
+
+		hSampleMassEff->Fill( m, v );
+		hSampleMassEffw->Fill( m );
+
+		// cout << "Eff( m="<<m << ", p=" << p << " ) = " << v << endl;
+
+		return 1.0 / ( v );
+
+	}
+
+	float tpcEfficiencyWeight( float m ){
+
+		TAxis * ax = hVPMassEff->GetXaxis();
+
+		int im = ax->FindBin( m );
+
+		float v = hVPMassEff->GetBinContent( im );
+		if ( 0 == v || 1.0/v != 1.0/v )
+			return 0;
+
+		// cout << "E1D (pT = " << pt << " ) = " << v << endl;
+
+		return 1.0 / ( v );
+
+	}
+
+	float tpcEfficiencyWeightSL( float pt ){
+
+		TAxis * ax = hSLMassEff->GetXaxis();
+
+		int ipt = ax->FindBin( pt );
+
+		float v = hSLMassEff->GetBinContent( ipt );
+		if ( 0 == v || 1.0/v != 1.0/v )
+			return 0;
+
+		return 1.0 / ( v );
+
+	}
+
+	float tpcEfficiencyWeightSL( float p, float m ){
+
+		TAxis * ax = hSLEff->GetXaxis();
+		TAxis * ay = hSLEff->GetYaxis();
+
+		int ip = ax->FindBin( p );
+		int im = ay->FindBin( m );
+
+		float v = hSLEff->GetBinContent( ip, im );
+		if ( 0 == v || 1.0/v != 1.0/v )
+			return 0;
+
+		return 1.0 / ( v );
+
+	}
+
+
+	float tpcEfficiencyWeightCosTheta( float m, float costheta ){
+
+		TAxis * ax = hCosThetaEff->GetXaxis();
+		TAxis * ay = hCosThetaEff->GetYaxis();
+
+		int ix = ax->FindBin( m );
+		int iy = ay->FindBin( costheta );
+
+		float v = ( hCosThetaEff->GetBinContent( ix, iy ) );
+		if ( 0 == v || 1.0/v != 1.0/v )
+			return 0;
+
+		return 1.0 / v;
 
 	}
 
@@ -170,15 +299,19 @@ protected:
 
 		double c_light = 29.9792458; // m / ns
 		double m_electron = 0.0005109990615;
+		// double m_electron = 0.0005109990615;
 		// double m_electron = 0.139;
 
 		
 		book->fill( "ZDCEast", pair->mZDCEast );
 		book->fill( "ZDCWest", pair->mZDCWest );
 		book->fill( "ZDCEastWest", pair->mZDCEast, pair->mZDCWest );
-		if ( pair->mZDCEast < ZDCEastMin || pair->mZDCEast > ZDCEastMax || pair->mZDCWest < ZDCWestMin || pair->mZDCWest > ZDCWestMax ) return;
+		// if ( pair->mZDCEast < ZDCEastMin || pair->mZDCEast > ZDCEastMax || pair->mZDCWest < ZDCWestMin || pair->mZDCWest > ZDCWestMax ) return;
 		// if ( abs(pair->mVertexZ) > 50 ) return;
 		book->fill( "ZDCEastWestSelected", pair->mZDCEast, pair->mZDCWest );
+
+		if ( fabs(pair->mVertexZ) > 100 ) return;
+		// if ( fabs(pair->mVertexZ) > 30 ) return;
 
 		book->fill( "events", 1 );
 		if ( pair->mGRefMult > 4 ) return;
@@ -186,8 +319,13 @@ protected:
 
 		// if ( pair->d1_mMatchFlag == 0 || pair->d2_mMatchFlag == 0 ) return;
 		if ( pair->d1_mPt  < 0.200 || pair->d2_mPt < 0.200 || abs( pair->d1_mEta ) > 1.0 || abs( pair->d2_mEta ) > 1.0 ) return;
-		if ( pair->d1_mDCA > 1.0 ) return; 
-		if ( pair->d2_mDCA > 1.0 ) return; 
+		// if ( abs( pair->d1_mEta ) > 1.0 || abs( pair->d2_mEta ) > 1.0 ) return;
+		
+		if ( pair->d1_mNHitsFit < min_nHitsFit || pair->d2_mNHitsFit < min_nHitsFit ) return;
+		if ( pair->d1_mNHitsDedx < min_nHitsDedx || pair->d2_mNHitsDedx < min_nHitsDedx ) return;
+		
+		if ( pair->d1_mDCA > max_Dca ) return; 
+		if ( pair->d2_mDCA > max_Dca ) return; 
 
 		book->fill( "events", 3 );
 
@@ -212,7 +350,7 @@ protected:
 		// 	return;
 
 		if ( abs(dTof) < 0.0001 || pair->d1_mMatchFlag == 0 || pair->d2_mMatchFlag == 0 ){
-			dTof = 999;
+			dTof = 9999;
 			dTofe = 399;
 		}
 
@@ -228,7 +366,7 @@ protected:
 			
 			book->fill( "chipid", xpipi, xee );
 
-			if ( xee < XeeCut ){
+			if ( xee < XeeCut && XeeXpipi * xee < xpipi ){
 				book->fill( "ulsid", lv.M(), lv.Pt(), ScaleFactor );
 				book->fill( "chipidSel", xpipi, xee );
 
@@ -282,17 +420,39 @@ protected:
 
 				book->fill( "dedxcut", lv1.P(), pair->d1_mNSigmaElectron );
 				book->fill( "dedxcut", -lv2.P(), pair->d2_mNSigmaElectron );
+				book->fill( "nSigE1_nSigE2_cut", pair->d1_mNSigmaElectron, pair->d2_mNSigmaElectron );
 			}
 
-			if ( xee < XeeCut && abs(dTof - dTofe) < dTofCut ){
+			book->fill( "nSigE1_nSigE2", pair->d1_mNSigmaElectron, pair->d2_mNSigmaElectron );
+
+
+			if ( xee < XeeCut && abs(dTof - dTofe) < dTofCut && XeeXpipi * xee < xpipi ){
+				book->fill( "nSigE1_nSigE2_sel", pair->d1_mNSigmaElectron, pair->d2_mNSigmaElectron );
 				book->fill( "ZDCEastWestAnalyzed", pair->mZDCEast, pair->mZDCWest );
 				book->fill( "ulsid2", lv.M(), lv.Pt(), ScaleFactor );
 				book->fill( "rapid2", lv.M(), lv.Rapidity(), ScaleFactor );
 				book->fill( "pt2id2", lv.M(), lv.Pt() * lv.Pt(), ScaleFactor );
 
-				book->fill( "wulsid2", lv.M(), lv.Pt(), tpcEfficiencyWeight( lv.Pt(), lv.M() ) );
+				book->fill( "mvsephi", lv1.Phi(), lv.M() );
+				book->fill( "mvspphi", lv2.Phi(), lv.M() );
+				book->fill( "mvsdphi", lv1.DeltaPhi( lv2 ), lv.M() );
+
+				book->fill( "wulsid2", lv.M(), lv.Pt(), tpcEfficiencyWeightSL( lv.Pt(), lv.M() ) );
+				book->fill( "w1ulsid2", lv.M(), lv.Pt(), tpcEfficiencyWeightSL( lv.M() ) );
+
+				book->fill( "wvpulsid2", lv.M(), lv.Pt(), tpcEfficiencyWeight( lv.Pt(), lv.M() ) );
+				book->fill( "wvp1ulsid2", lv.M(), lv.Pt(), tpcEfficiencyWeight( lv.M() ) );
 				book->fill( "wrapid2", lv.M(), lv.Rapidity(), tpcEfficiencyWeight( lv.Pt(), lv.M() ) );
 				book->fill( "wpt2id2", lv.M(), lv.Pt() * lv.Pt(), tpcEfficiencyWeight( lv.Pt(), lv.M() ) );
+
+				if ( pair->d1_mEta > 0 )
+					book->fill( "mNHits_mPhi_etap_pos", pair->d1_mPhi, pair->d1_mNHitsFit );
+				else 
+					book->fill( "mNHits_mPhi_etan_pos", pair->d1_mPhi, pair->d1_mNHitsFit );
+				if ( pair->d2_mEta > 0 )
+					book->fill( "mNHits_mPhi_etap_neg", pair->d2_mPhi, pair->d2_mNHitsFit );
+				else 
+					book->fill( "mNHits_mPhi_etan_neg", pair->d2_mPhi, pair->d2_mNHitsFit );
 
 				// LOG_F( INFO, "3D eff factor (%f, %f, %f) = %f", lv.Pt(), lv.M(), lv.Rapidity(), tpcEfficiencyWeight( lv.Pt(), lv.M(), lv.Rapidity() ) );
 				float e3d = tpcEfficiencyWeight( lv.Pt(), lv.M(), lv.Rapidity() );
@@ -308,6 +468,7 @@ protected:
 				float costheta = cos( vPositron.Angle( vbeam ) );
 
 				book->fill( "w3costhetaid2", lv.M(), abs(costheta), tpcEfficiencyWeight( lv.Pt(), lv.M() ) );
+				book->fill( "w2costhetaid2", lv.M(), abs(costheta), tpcEfficiencyWeightCosTheta( lv.M(), abs(costheta) ) );
 
 
 				book->fill( "chipidcutSel", xpipi, xee );
@@ -318,7 +479,7 @@ protected:
 				book->fill( "aco", lv.M(), aco );
 				book->fill( "asym", lv.M(), asym );
 
-				if ( lv.M() > 0.4 && lv.M() < 3.0 ){
+				if ( lv.M() > 0.4 && lv.M() < 0.76 ){
 					
 					TVector3 vParent( lv.Px(), lv.Py(), lv.Pz() );
 					TVector3 vD1( lv1.Px(), lv1.Py(), lv1.Pz() );
@@ -327,17 +488,35 @@ protected:
 					book->fill( "deltaPhi_pd1", lv1.DeltaPhi( lv ) );
 					book->fill( "deltaPhi_pd2", lv.DeltaPhi( lv2 ) );
 
+					book->fill( "deltaPhi_pd1_fold", foldPi(lv1.DeltaPhi( lv )) );
+
+					book->fill( "wdeltaPhi_pd1", lv1.DeltaPhi( lv ), tpcEfficiencyWeight( lv.Pt(), lv.M() ) );
+					book->fill( "wdeltaPhi_pd2", lv.DeltaPhi( lv2 ), tpcEfficiencyWeight( lv.Pt(), lv.M() ) );
+
+					book->fill( "wdeltaPhi_pd1_fold", foldPi(lv1.DeltaPhi( lv )), tpcEfficiencyWeight( lv.Pt(), lv.M() ) );
+					book->fill( "wdeltaPhi_pd1_2fold", foldPi2(lv1.DeltaPhi( lv )), tpcEfficiencyWeight( lv.Pt(), lv.M() ) );
+
+
 					book->fill( "deltaAngle_pd1", vD2.Angle( vD1 ) );
 					book->fill( "deltaAngle_pd2", vParent.Angle( vD2 ) );
 
-					// if ( lv.Pt() < 0.01 ) {
-					// 	LOG_F( INFO, "d1: pt = %f, px = %f, %f, %f", lv1.Pt(), lv1.Px(), lv1.Py(), lv1.Pz() );
-					// 	LOG_F( INFO, "d2: pt = %f, px = %f, %f, %f", lv2.Pt(), lv2.Px(), lv2.Py(), lv2.Pz() );
-					// }
+					book->fill( "wdeltaPhi_pd1_fold_pt", lv.Pt(), foldPi(lv1.DeltaPhi( lv )), tpcEfficiencyWeight( lv.Pt(), lv.M() ) );
+
 				}
+
+				if ( lv.M() > 0.4 && lv.M() < 3.0 ){
+					book->fill( "wdeltaPhi_pd1_fold_all", foldPi(lv1.DeltaPhi( lv )), tpcEfficiencyWeight( lv.Pt(), lv.M() ) );
+					book->fill( "wdeltaPhi_pd1_2fold_all", foldPi2(lv1.DeltaPhi( lv )), tpcEfficiencyWeight( lv.Pt(), lv.M() ) );
+				}
+
+				book->fill( "deltaPhi_pd1_fold_mass", lv.M(), foldPi(lv1.DeltaPhi( lv )) );
+				book->fill( "wdeltaPhi_pd1_fold_mass", lv.M(), foldPi(lv1.DeltaPhi( lv )), tpcEfficiencyWeight( lv.Pt(), lv.M() ) );
 
 				book->fill( "mDCA", pair->d1_mDCA );
 				book->fill( "mDCA", pair->d2_mDCA );
+
+				book->fill( "mDCA_mPt", pair->d1_mPt, pair->d1_mDCA );
+				book->fill( "mDCA_mPt", pair->d2_mPt, pair->d2_mDCA );
 
 				book->fill( "mNHitsFit", pair->d2_mNHitsFit );
 				book->fill( "mNHitsFit", pair->d2_mNHitsFit );
@@ -360,6 +539,22 @@ protected:
 
 
 	} //analyzeEvent
+
+	double foldPi( double phi ){
+		double aphi = fabs( phi );
+		if ( aphi > M_PI/2 )
+			aphi = aphi - M_PI/2;
+		return aphi;
+	}
+
+	double foldPi2( double phi ){
+		double aphi = fabs( phi );
+		if ( aphi > M_PI/2.0 )
+			aphi =  aphi - M_PI/2;
+		if ( aphi > M_PI/4.0 )
+			aphi = M_PI/4.0 - aphi;
+		return aphi;
+	}
 
 
 	void expoFit( TH1 * h, float *mean, float *error ){
